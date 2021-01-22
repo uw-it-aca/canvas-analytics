08498fd61a9ad0668b4c8fec9d2dbadfdcb579b6
Jim Laney <jlaney@uw.edu>
2021-01-22 12:12:10 -0800

    adds webpack_loader config

--- docker/settings.py
+++ docker/settings.py
@@ -12,3 +12,10 @@ if os.getenv('ENV') == 'localdev':
     RESTCLIENTS_DAO_CACHE_CLASS = None
 else:
     DATA_AGGREGATOR_ACCESS_GROUP = os.getenv('ACCESS_GROUP', '')
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'BUNDLE_DIR_NAME': 'data_aggregator/bundles/',
+        'STATS_FILE': os.path.join(BASE_DIR, 'data_aggregator', 'static', 'webpack-stats.json'),
+    }
+}
