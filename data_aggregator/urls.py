c9d18b23031cada66180da2a8a7288f5d107c813
Nicholas Falco <ncfalco@uw.edu>
2021-02-05 14:31:16 -0600

    Change internal jobs api endpoint

--- data_aggregator/urls.py
+++ data_aggregator/urls.py
@@ -15,15 +15,15 @@ Including another URLconf
 """
 from django.urls import re_path, include
 from data_aggregator.views.pages import HomeView
-from data_aggregator.views.api.data import JobFilter, JobReset
+from data_aggregator.views.api.jobs import JobView, JobRestartView
 from data_aggregator.views.api.analytics import AccountAssignmentView, \
     AccountParticipationView, TermAssignmentView, TermParticipationView, \
     StudentAssignmentView, StudentParticipationView
 
 urlpatterns = [
     re_path(r'^$', HomeView.as_view()),
-    re_path(r'^api/filterjobs/$', JobFilter.as_view()),
-    re_path(r'^api/resetjobs/$', JobReset.as_view()),
+    re_path(r'^api/internal/jobs/$', JobView.as_view()),
+    re_path(r'^api/internal/jobs/restart/$', JobRestartView.as_view()),
     re_path(r'^api/(?P<version>v[1])/account/(?P<sis_account_id>[-@:\w]+)/'
             r'assignment/$',
             AccountAssignmentView.as_view()),
