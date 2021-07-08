38b1dcd9a38f323df9ef06f4a74e9fc86bfbdb08
Nicholas Falco <ncfalco@uw.edu>
2021-07-07 19:51:51 -0500

    Add license

--- data_aggregator/management/commands/build_subaccount_activity_report.py
+++ data_aggregator/management/commands/build_subaccount_activity_report.py
@@ -1,3 +1,6 @@
+# Copyright 2021 UW-IT, University of Washington
+# SPDX-License-Identifier: Apache-2.0
+
 from data_aggregator.report_builder import ReportBuilder
 from django.core.management.base import BaseCommand
 
