4c78de127dcb400e60b4b50a1bd4f2f6024ae7e7
Nicholas Falco <ncfalco@uw.edu>
2021-07-23 17:46:11 -0500

    Add test_create_terms

--- data_aggregator/tests/test_dao.py
+++ data_aggregator/tests/test_dao.py
@@ -675,6 +675,30 @@ class TestTaskDAO(TestCase):
         td = TaskDAO()
         return td
 
+    def mock_get_or_create_from_sws_term(self, sws_term):
+        term = MagicMock()
+        term.sis_term_id = sws_term.sis_term_id
+        return term, True
+
+    @patch('data_aggregator.dao.Term')
+    def test_create_terms(self, mock_term_model):
+        td = self.get_test_task_dao()
+        mock_term1 = MagicMock()
+        mock_term1.sis_term_id = "2021-spring"
+        mock_term2 = MagicMock()
+        mock_term2.sis_term_id = "2021-summer"
+        mock_terms = [mock_term1, mock_term2]
+        td.get_sws_terms = MagicMock(return_value=mock_terms)
+        mock_term_model.objects.get_or_create_from_sws_term = \
+            MagicMock(side_effect=self.mock_get_or_create_from_sws_term)
+        td.create_terms()
+        call_args_list = (
+            mock_term_model.objects.get_or_create_from_sws_term
+            .call_args_list)
+        assert (call_args_list[0] == call(mock_term1))
+        assert (call_args_list[1] == call(mock_term2))
+        self.assertEqual(len(call_args_list), 2)
+
     def test_create_or_update_courses(self):
         td = self.get_test_task_dao()
         mock_course_provisioning_file = \
